9a3dd6e7f515b0cdd8dae2101d8181ce1ce76d01
Geekbooi <yosephmekonnen@gmail.com>
2023-01-05 23:54:53 -0600

    backend alx python

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -5,12 +5,10 @@ and returns either none or anydatatype
 """
 from typing import Any, Mapping, TypeVar, Union
 
-
 T = TypeVar("T")
 
-
 def safely_get_value(dct: Mapping, key: Any,
-                     default: Union[T, None]) -> Union[Any, T]:
+        default: Union[T, None]) -> Union[Any, T]:
     """Function to return a value from a dictionary using its"""
     if key in dct:
         return dct[key]
